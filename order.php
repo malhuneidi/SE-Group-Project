df5db39d9e8980b3510f9b5b54f3bdb3c7bdef9b
danwo_000 <danwo_000@RIG>
2017-02-28 20:23:09 -0600

    Revert "fix page titles"
    This reverts commit 2bb1c109bae9093519c8f358ab98786cab0174f5.

--- order.php
+++ order.php
@@ -1,100 +1,78 @@
 <!DoCTYPE html>
 <html>
-<title>Order Now</title>
+<title>Order ThaiFood now</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
-<link rel="stylesheet"
-	href="http://fonts.googleapis.com/css?family=Raleway">
-<link rel="stylesheet"
-	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-html, body {
-	padding: 0;
-	height: 100%;
+html,body {padding:0; height:100%;}
+body,h1 {font-family: "Raleway", Arial, sans-serif}
+h1 {letter-spacing: 6px}
+.w3-row-padding img {margin-bottom: 12px}
+body{
+        overflow-x:hidden;
 }
 
-body, h1 {
-	font-family: "Raleway", Arial, sans-serif
+input[type=text ]{
+width:330px;
+      box-sizing: border-box;
+border: 2px solid #ccc;
+        border-radius:4px;
+        font-size:16px;
+        background-color: grey;
+        background-image: url('searchicon.png');
+        background-position:10px 10px;
+        background-repeat: no-repeat;
+padding : 12px 20px 12px 12px;
+          -webkit-transition: width 0.4s ease-in-out;
+transition: width 0.4s ease-in-out;
 }
-
-h1 {
-	letter-spacing: 6px
+footer{
+width:100%;
+position:absolute;
+bottom:0;
 }
-
-.w3-row-padding img {
-	margin-bottom: 12px
-}
-
-body {
-	overflow-x: hidden;
-}
-
-input[type=text] {
-	width: 330px;
-	box-sizing: border-box;
-	border: 2px solid #ccc;
-	border-radius: 4px;
-	font-size: 16px;
-	background-color: grey;
-	background-image: url('searchicon.png');
-	background-position: 10px 10px;
-	background-repeat: no-repeat;
-	padding: 12px 20px 12px 12px;
-	-webkit-transition: width 0.4s ease-in-out;
-	transition: width 0.4s ease-in-out;
-}
-
-footer {
-	width: 100%;
-	position: absolute;
-	bottom: 0;
-}
-
-input[type=text]:focus {
-	width: 50%;
+input[type=text]:focus{
+width:50%;
 }
 </style>
 
 <body>
-	<div class="w3-container w3-padding-44 w3-light-green w3-center">
+<div class="w3-container w3-padding-44 w3-light-green w3-center">
 
-		<div class="w3-padding-62">
-			<div class="w3-btn-bar w3-border w3-show-inline-block w3-left">
-				<a href="index.php" class="w3-btn w3-border w3-xlarge w3-opacity ">Homepage</a>
+<div class="w3-padding-62">
+<div class="w3-btn-bar w3-border w3-show-inline-block w3-left">
+<a href="index.php" class="w3-btn w3-border w3-xlarge w3-opacity ">Homepage</a>
 
-				<a href="order.php" class="w3-btn w3-border w3-xlarge w3-opacity ">Order
-					Now</a> <a href="contactus.php"
-					class="w3-btn w3-border w3-xlarge w3-opacity ">Contact Us</a>
+<a href="order.php" class="w3-btn w3-border w3-xlarge w3-opacity ">Order Now</a>
+<a href="contactus.php" class="w3-btn w3-border w3-xlarge w3-opacity ">Contact Us</a>
 
-			</div>
-		</div>
+</div>
+</div>
 
-	</div>
-	<!-- !PAGE CONTENT! -->
+</div>
+<!-- !PAGE CONTENT! -->
 
-	<!-- Header -->
-	<header class="w3-panel w3-padding-228 w3-center w3-opacity">
-		<h1 class="w3-xlarge">Order ThaiFood</h1>
-		<h1>Order your favorite dishes for delivery!</h1>
-	</header>
-	<div class="w3-btn-bar  w3-center">
-		<p>Thank you for taking the chance to take a look at our website !</p>
-		<p>
-			You can start ordering your favorite Thai Dishes by Clicking <a
-				href="order.php">Here</a>
-		</p>
+<!-- Header -->
+<header class="w3-panel w3-padding-228 w3-center w3-opacity">
+<h1 class="w3-xlarge">Order ThaiFood</h1>
+<h1>Order your favorite dishes for delivery!</h1>
+</header>
+<div class="w3-btn-bar  w3-center">
+<p> Thank you for taking the chance to take a look at our website !</p>
+<p> You can start ordering your favorite Thai Dishes by Clicking <a href="order.php">Here</a> </p>
 
-	</div>
+</div>
 
 
-	<!-- Footer -->
-	<footer class=" w3-padding-64 w3-light-green w3-center">
-		<a href="#" class="w3-hover-text-indigo"><i
-			class="fa fa-facebook-official"></i></a> <a href="#"
-			class="w3-hover-text-light-blue"><i class="fa fa-twitter"></i></a>
-		<p>Powered by Amazon's Web Service Server</p>
-	</footer>
+<!-- Footer -->
+<footer class=" w3-padding-64 w3-light-green w3-center">
+<a href="#" class="w3-hover-text-indigo"><i class="fa fa-facebook-official"></i></a>
+<a href="#" class="w3-hover-text-light-blue"><i class="fa fa-twitter"></i></a>
+<p>Powered by Amazon's Web Service Server</p>
+</footer>
 </body>
 </html>
 
